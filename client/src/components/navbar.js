ee48db5777eee8ce03148b7ceb242ee4a2439832
eshaalal <eshalal888@gmail.com>
2024-07-15 15:45:34 +0530

    addedcss

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
+import "./navbar.css";  // Import the CSS file
 
 export const Navbar = () => {
   const [cookies, setCookies] = useCookies(["access_token"]);
@@ -11,16 +12,22 @@ export const Navbar = () => {
     window.localStorage.clear();
     navigate("/auth");
   };
+
   return (
     <div className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/create-recipe">Create Recipe</Link>
-      <Link to="/saved-recipes">Saved Recipes</Link>
-      {!cookies.access_token ? (
-        <Link to="/auth">Login/Register</Link>
-      ) : (
-        <button onClick={logout}> Logout </button>
-      )}
+      <div className="brand">
+        Recipe Delight
+      </div>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/create-recipe">Create Recipe</Link>
+        <Link to="/saved-recipes">Saved Recipes</Link>
+        {!cookies.access_token ? (
+          <Link to="/auth">Login/Register</Link>
+        ) : (
+          <button onClick={logout}>Logout</button>
+        )}
+      </div>
     </div>
   );
 };
